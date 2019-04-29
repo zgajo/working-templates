f43221aacc39027e978048278fcef97ab4fef3c3
Darko Pranjic <dpranjic@iolap.com>
2019-04-29 15:55:59 +0200

    Read folders and based on answers generate template

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 // npm link to make package global - This is only possible because of the first line of the script: #!/usr/bin/env node
 
+const fs = require("fs");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const figlet = require("figlet");
-const shell = require("shelljs");
 
 const init = () => {
   console.log(
     chalk.green(
-      figlet.textSync("Node f*cking JS", {
+      figlet.textSync("Generate Template", {
         font: "Ghost",
         horizontalLayout: "default",
         verticalLayout: "default"
@@ -18,50 +18,84 @@ const init = () => {
   );
 };
 
+const CURR_DIR = process.cwd();
+
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+
 const askQuestions = () => {
-  const questions = [
-    {
-      choices: ["frontend", "backend"],
-      name: "FILENAME",
+  return inquirer
+    .prompt({
+      choices: CHOICES,
+      name: "TEMPLATE_FOR",
       type: "list",
-      message: "What template whould do you want?"
-    },
-    {
-      type: "rawlist",
-      name: "EXTENSION",
-      message: "What is the file extension?",
-      choices: [".rb", ".js", ".php", ".css"],
-      filter: function(val) {
-        return val.split(".")[1];
-      }
+      message: "What template do you want?"
+    })
+    .then(({ TEMPLATE_FOR }) =>
+      inquirer
+        .prompt({
+          type: "rawlist",
+          name: "TEMPLATE",
+          message: "What is the template you want to use?",
+          choices: fs.readdirSync(`${__dirname}/templates/${TEMPLATE_FOR}`)
+        })
+        .then(({ TEMPLATE }) => {
+          createDirectoryContents(
+            `${__dirname}/templates/${TEMPLATE_FOR}/${TEMPLATE}`,
+            TEMPLATE
+          );
+
+          return `${__dirname}/${TEMPLATE}`;
+        })
+    );
+};
+
+function createDirectoryContents(templatePath, newProjectPath) {
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach(file => {
+    const origFilePath = `${templatePath}/${file}`;
+
+    if (file === "node_modules") {
+      return;
     }
-  ];
-  return inquirer.prompt(questions);
-};
 
-const createFile = (filename, extension) => {
-  const filePath = `${process.cwd()}/${filename}.${extension}`;
-  shell.touch(filePath);
-  return filePath;
-};
+    // Folder is new
+    if (!fs.existsSync(newProjectPath)) {
+      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}`);
+    }
 
-const success = filepath => {
-  console.log(chalk.white.bgGreen.bold(`Done! File created at ${filepath}`));
+    // get stats about the current file
+    const stats = fs.statSync(origFilePath);
+
+    if (stats.isFile()) {
+      const contents = fs.readFileSync(origFilePath, "utf8");
+
+      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+      fs.writeFileSync(writePath, contents, "utf8");
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+
+      // recursive call
+      createDirectoryContents(
+        `${templatePath}/${file}`,
+        `${newProjectPath}/${file}`
+      );
+    }
+  });
+}
+
+const success = folderPath => {
+  console.log(chalk.white.bgGreen.bold(`Done! File created at ${folderPath}`));
 };
 
 const run = async () => {
   // show script introduction
   init();
 
-  // ask questions
-  const answers = await askQuestions();
-  const { FILENAME, EXTENSION } = answers;
-
-  // create the file
-  const filePath = createFile(FILENAME, EXTENSION);
+  const folderPath = await askQuestions();
 
   // show success message
-  success(filePath);
+  success(folderPath);
 };
 
 run();
